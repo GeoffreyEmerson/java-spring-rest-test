1b68d467e919b3cf8cd555d62df95262c9e23e61
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2017-04-20 23:12:00 -0700

    add more demo data

--- src/main/java/com/teamtreehouse/core/DatabaseLoader.java
+++ src/main/java/com/teamtreehouse/core/DatabaseLoader.java
@@ -8,6 +8,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Component
 public class DatabaseLoader implements ApplicationRunner{
     private final CourseRepository courses;
@@ -21,6 +25,36 @@ public class DatabaseLoader implements ApplicationRunner{
     public void run(ApplicationArguments args) throws Exception {
         Course course = new Course("Java Basics","https://teamtreehouse.com/library/java-basics");
         course.addReview(new Review(3,"You ARE a dork!!!"));
+
         courses.save(course);
+
+        String[] templates = {
+                "Up and Running with %s",
+                "%s Basics",
+                "%s for Beginners",
+                "%s for Neckbeards",
+                "Under the Hood: %s"
+        };
+
+        String[] buzzwords = {
+                "Spring REST Data",
+                "Java 9",
+                "Scala",
+                "Groovy",
+                "Hibernate"
+        };
+
+        List<Course> bunchOfCourses = new ArrayList<>();
+        IntStream.range(1, 100)
+        .forEach(i -> {
+            String template = templates[i % templates.length];
+            String buzzword = buzzwords[i % buzzwords.length];
+            String title = String.format(template,buzzword);
+            Course c = new Course(title, "http://www.example.com");
+            c.addReview(new Review(i % 5, String.format("Moar %s plz!!!", buzzword)));
+            bunchOfCourses.add(c);
+        });
+
+        courses.save(bunchOfCourses);
     }
 }
